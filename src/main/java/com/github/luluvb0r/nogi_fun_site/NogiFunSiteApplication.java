d201a81b9960289733a1095252d7bb0eed22b3a8
luluvb0r <seipon1217@docomo.ne.jp>
2025-08-16 18:15:49 +0900

    first commit

---
+++ src/main/java/com/github/luluvb0r/nogi_fun_site/NogiFunSiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.luluvb0r.nogi_fun_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NogiFunSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NogiFunSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/luluvb0r/nogi_fun_site/NogiFunSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.luluvb0r.nogi_fun_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NogiFunSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
